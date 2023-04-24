ce81f39dd4569629305bb06e11358e94c5dc86c5
Agénor Debriat <agenor.debriat@me.com>
2023-04-24 02:23:22 +0200

    ➕ add UnoCSS transformers and ESLint support

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig, presetUno } from "unocss"
 import formKitUnocss from "@formkit/themes/unocss"
+import transformerDirectives from "@unocss/transformer-directives"
+import transformerVariantGroup from "@unocss/transformer-variant-group"
+import { defineConfig, presetUno } from "unocss"
 
 export default defineConfig({
   presets: [formKitUnocss(), presetUno()],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
 })
